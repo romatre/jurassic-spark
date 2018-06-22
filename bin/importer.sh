663258384c9309d30e87efc23f5133382085f8f6
menxit <federico@menxit.com>
2018-06-22 15:00:47 +0200

    fix pice -> price

--- bin/importer.sh
+++ bin/importer.sh
@@ -3,6 +3,6 @@ mongoimport \
     --collection transactions \
     --file $1 \
     --type csv \
-    --fields "hashTx.auto(),timestamp.auto(),blockNumber.auto(),from.auto(),to.auto(),gas.auto(),gasPice.auto(),value.auto()" \
+    --fields "hashTx.auto(),timestamp.auto(),blockNumber.auto(),from.auto(),to.auto(),gas.auto(),gasPrice.auto(),value.auto()" \
     --columnsHaveTypes \
     --numInsertionWorkers 8
