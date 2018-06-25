3c9e73d183a84ca229beb88129bfef4417966981
menxit <federico@menxit.com>
2018-06-25 23:12:20 +0200

    config

--- streamer/config/production.js
+++ streamer/config/production.js
@@ -1,5 +1,1 @@
-module.exports = {
-  RABBITMQ_HOST: 'amqp://localhost:5672',
-  INITIAL_BLOCK: 5758000,
-  WEB3_HOST: 'https://mainnet.infura.io/Ey1sRQVAgNXc3tbwLGwA'
-};
+module.exports = {};
--- writer/config/production.js
+++ writer/config/production.js
@@ -1,7 +1,1 @@
-module.exports = {
-  RABBITMQ_HOST: 'amqp://localhost:5672',
-  MONGODB_URI: 'mongodb://localhost:27017',
-  MONGODB_DB: 'jurassicspark',
-  COLLECTION_OUTPUT: 'transactions',
-  QUEQUE_NAME: 'transactions'
-};
+module.exports = {};
